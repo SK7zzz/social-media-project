9b79f5f7739e10c686bf18a1361fb49312175793
= <=>
2022-09-15 21:47:02 +0200

    Project set up and firebase conections done

---
+++ src/pages/create-post/create-post.tsx
@@ -0,0 +1,11 @@
+import CreateForm from "./create-form"
+
+const CreatePost = () => {
+    return (
+        <div>
+            <CreateForm></CreateForm>
+        </div>
+    )
+}
+
+export default CreatePost
---
+++ src/pages/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <div>{process.env.REACT_APP_API_KEY}</div>
+    )
+}
+
+export default Main
